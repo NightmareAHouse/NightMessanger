0247dc8014d544525a35053332d67e2ff9475076
NightmareAHouse <55136609+nightmareahouse@users.noreply.github.com>
2022-10-27 10:31:45 +0300

    NM-2 Add chat design

--- src/component/chat/Chat.tsx
+++ src/component/chat/Chat.tsx
@@ -1,30 +1,37 @@
-import {Box, IconButton, InputBase, Paper} from "@mui/material"
+import {Box, Grid, IconButton, InputBase, Paper} from "@mui/material"
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SettingsSharpIcon from '@mui/icons-material/SettingsSharp';
 
 const Chat = () => {
     return (
         <>
-            <Box bgcolor={'#25334e78'} minHeight={'6vh'} width={'100%'} display={'flex'}>
+            <Box bgcolor={'#25334e78'} minHeight={'7vh'} width={'100%'} display={'flex'}>
                 <Paper sx={{display: "flex", width: '100%', backgroundColor: '#25334e78'}}>
                     <Box padding={1} color={'white'}>
-                        Test Testovich
+                        <Grid xs={25} fontSize={22}>
+                            Test testovich
+                        </Grid>
+                        <Grid xs={25} fontSize={12}>
+                            Was online 2 hours ago
+                        </Grid>
                     </Box>
                     <Box sx={{ml: 1, flex: 1}}/>
                     <Box sx={{p: '8px'}}>
-                        <IconButton color="primary"  aria-label="directions">
-                            <SentimentSatisfiedAltIcon/>
+                        <IconButton color="primary" aria-label="directions">
+                            <SettingsSharpIcon/>
                         </IconButton>
-                        <IconButton color="primary"  aria-label="directions">
-                            <SendOutlinedIcon />
+                        <IconButton color="primary" aria-label="directions">
+                            <SearchOutlinedIcon/>
                         </IconButton>
                     </Box>
                 </Paper>
             </Box>
 
-            <Box minHeight={'89vh'}>
-                Chat
+            <Box minHeight={'88vh'}>
+
             </Box>
 
             <Box bgcolor={'#25334e78'} minHeight={'5vh'} sx={{textAlign: '-webkit-center'}}>
@@ -38,7 +45,9 @@ const Chat = () => {
                     />
                     <IconButton color="primary" sx={{p: '10px'}} aria-label="directions">
                         <SentimentSatisfiedAltIcon/>
-                        <SendOutlinedIcon sx={{marginLeft: 2}}/>
+                    </IconButton>
+                    <IconButton color="primary" sx={{p: '10px'}} aria-label="directions">
+                        <SendOutlinedIcon/>
                     </IconButton>
                 </Paper>
             </Box>
