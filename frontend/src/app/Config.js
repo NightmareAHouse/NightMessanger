a5bf7f36a1e3869a1e6d31014317e26bc0ae638f
NightmareAHouse <55136609+nightmareahouse@users.noreply.github.com>
2022-11-21 10:42:17 +0200

    Main - update dockerfile

--- frontend/src/app/Config.js
+++ frontend/src/app/Config.js
@@ -1,4 +1,4 @@
-let API_URL = "http://localhost:5000/";
+let API_URL = "http://113.30.189.131:5000/";
 
 if (process.env.NODE_ENV === "production") {
     API_URL = `http://${window.location.origin}:5000`
