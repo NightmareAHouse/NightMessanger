7fc1195cb419c93d3f94b1c94c3126fe05210b28
NightmareAHouse <55136609+nightmareahouse@users.noreply.github.com>
2022-11-22 15:01:32 +0200

    Main - update package.json and config

--- frontend/src/app/Config.js
+++ frontend/src/app/Config.js
@@ -1,7 +1,3 @@
-let API_URL = "http://localhost:5000";
-
-if (process.env.NODE_ENV === "production") {
-    API_URL = `http://localhost:5000`
-}
+let API_URL = "http://185.229.224.9:5000";
 
 export { API_URL };
