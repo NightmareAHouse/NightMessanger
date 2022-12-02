c9c155b79c18b3fba480a6e61314bff138589873
NightmareAHouse <55136609+nightmareahouse@users.noreply.github.com>
2022-12-02 13:15:18 +0200

    NM-08 Save data

--- frontend/src/component/chat-list/ChatList.tsx
+++ frontend/src/component/chat-list/ChatList.tsx
@@ -1,37 +1,54 @@
-import {Avatar, Box, Grid} from "@mui/material";
+import {Avatar, Box, ClickAwayListener, Grid} from "@mui/material";
+import {useState} from "react";
 
 const ChatList = (props: {
     users: never[],
 }) => {
     const {users} = props;
+    const [open, setOpen] = useState(false);
 
     const activeUsers = Object.values(users);
+
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+    };
+
+    const handleClickAway = () => {
+        setOpen(false);
+    };
 
     return (
         <>
             {activeUsers.map((e: any, index) => {
                 return (
-                    <Box key={index.toString()} sx={{
-                        cursor: "pointer",
-                        '&:hover': {
-                            backgroundColor: '#263552',
-                            transitionDuration: '0.5s'
-                        }
-                    }}>
-                        <Grid key={index.toString()} container minHeight={40} padding={1}>
-                            <Grid item xs={1.8}>
-                                <Avatar>{e.username.charAt(0)}</Avatar>
+                    <>
+                        <Box key={index.toString()} sx={{
+                            cursor: "pointer",
+                            '&:hover': {
+                                backgroundColor: '#263552',
+                                transitionDuration: '0.5s'
+                            }
+                        }} onClick={handleClick}>
+                            <Grid key={index.toString()} container minHeight={40} padding={1}>
+                                <Grid item xs={1.8}>
+                                    <Avatar>{e.username.charAt(0)}</Avatar>
+                                </Grid>
+                                <Box>
+                                    <Grid item xs={12} fontSize={15}>
+                                        {e.username}
+                                    </Grid>
+                                    <Grid item xs={10} fontSize={15}>
+                                        {e.online ? "online" : "offline"}
+                                    </Grid>
+                                </Box>
                             </Grid>
-                            <Box>
-                                <Grid item xs={12} fontSize={15}>
-                                    {e.username}
-                                </Grid>
-                                <Grid item xs={10} fontSize={15}>
-                                    {e.online ? "online" : "offline"}
-                                </Grid>
+                        </Box>
+                        {open ? (
+                            <Box sx={{backgroundColor: 'red', width: 225, height: 175}}>
+                                Click me, I will stay visible until you click outside.
                             </Box>
-                        </Grid>
-                    </Box>
+                        ) : null}
+                    </>
                 )
             })}
         </>
